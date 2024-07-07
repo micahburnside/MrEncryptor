c6bbaad299b6019a78b65e024179a541f1645b95
Micah Burnside <micahburnside@protonmail.com>
2024-07-07 15:27:18 -0400

    add return values from Question constructor

--- Question.js
+++ Question.js
@@ -1,13 +1,19 @@
 class Question {
-constructor(type, name, message, choices) {
-  this.type = type;
-  this.name = name;
-  this.message = message;
-  this.choices = choices;
+
+  constructor(type, name, message, choices) {
+    this.type = type;
+    this.name = name;
+    this.message = message;
+    this.choices = choices;
   }
 
   static getMenuChoices() {
-
+    return {
+      type: 'list',
+      name: 'menuChoice',
+      message: 'Select an option from the menu',
+      choices: Question.menuChoices,
+    };
   }
 }
 
