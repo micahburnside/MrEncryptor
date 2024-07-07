de774b783dec890249cc375111042a19b2735cf0
Micah Burnside <micahburnside@protonmail.com>
2024-07-07 15:28:02 -0400

    add menuChoices array

--- Question.js
+++ Question.js
@@ -1,5 +1,25 @@
 class Question {
 
+  static menuChoices = ['choice 1', 'choice 2', 'choice 3', 'choice 4', 'choice 5', 'choice 6'];
+
+  constructor(type, name, message, choices) {
+    this.type = type;
+    this.name = name;
+    this.message = message;
+    this.choices = choices;
+  }
+
+  static getMenuQs() {
+    return {
+      type: 'list',
+      name: 'menuChoice',
+      message: 'Select an option from the menu',
+      choices: Question.menuChoices,
+    };
+  }
+}
+
+export default Question;
   constructor(type, name, message, choices) {
     this.type = type;
     this.name = name;
