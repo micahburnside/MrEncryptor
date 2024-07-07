82f51acf32614f27a8d30e22e55c692496d1a626
Micah Burnside <micahburnside@protonmail.com>
2024-07-07 15:16:34 -0400

    refactor Question constructor to accept type, name, message, choices parameters

--- Question.js
+++ Question.js
@@ -1,6 +1,10 @@
 class Question {
-constructor() {
-}
+constructor(type, name, message, choices) {
+  this.type = type;
+  this.name = name;
+  this.message = message;
+  this.choices = choices;
+  }
 }
 
 export default Question;
