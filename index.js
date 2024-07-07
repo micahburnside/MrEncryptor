d3cd7514f3d16eb4974247dbe6aeecf65d929362
Micah Burnside <micahburnside@protonmail.com>
2024-07-07 14:00:50 -0400

    add prompt and answer test statements

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ import inquirer from 'inquirer';
 inquirer
   .prompt([
     /* Pass your questions in here */
+    console.log("Question Test")
+
   ])
   .then((answers) => {
     // Use user feedback for... whatever!!
+    console.log("Answer Test")
   })
   .catch((error) => {
     if (error.isTtyError) {
@@ -15,3 +18,4 @@ inquirer
       // Something else went wrong
     }
   });
+
